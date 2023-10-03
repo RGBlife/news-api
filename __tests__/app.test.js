e7c5ae3cc578f5efdefa919dc3280710a0a6147f
Bret <bret_gomes811@hotmail.co.uk>
2023-10-03 12:14:51 +0100

    added assertion to check each property value of each article

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -129,7 +129,18 @@ describe("GET /api/articles", () => {
     const correctStructure = articles.every((article) => {
       return Object.keys(expected).every((key) => {
         return article.hasOwnProperty(key);
-      })
+      });
+    });
+
+    articles.forEach((article) => {
+      expect(typeof article.author).toBe("string");
+      expect(typeof article.title).toBe("string");
+      expect(typeof article.article_id).toBe("number");
+      expect(typeof article.topic).toBe("string");
+      expect(typeof article.created_at).toBe("string");
+      expect(typeof article.votes).toBe("number");
+      expect(typeof article.article_img_url).toBe("string");
+      expect(typeof article.comment_count).toBe("string");
     });
 
     expect(correctStructure).toBe(true);
