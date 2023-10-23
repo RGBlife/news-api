1875440c21e49b20f2c09a80d490f01610e4893e
Bret <bret_gomes811@hotmail.co.uk>
2023-10-23 09:19:33 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const {
   pathNotFoundError,
   handleCustomErrors,
@@ -8,6 +9,7 @@ const apiRouter = require("./routes/api-router");
 const express = require("express");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
